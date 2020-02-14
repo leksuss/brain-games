9bed05bbf58d9a8be4f91f4b3e11eca6e5147c95
Andrey <leksus@leksus.info>
2020-02-14 15:09:41 +0300

    added game even

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,8 +5,41 @@
 import prompt
 
 
-def welcome_user():
-    """Welcome message and get user name."""
-    print('Welcome to the Brain Games!')
+def welcome_user(description=''):
+    """Welcome message with game description and getting username.
+
+    Print welcome message and game rules and description, after that ask
+    user name.
+
+    Args:
+        description: The game rules and it's description.
+
+    Returns:
+        name of user
+    """
+    print_response('Welcome to the Brain Games!')
+    print_response(description)
     name = prompt.string('May I have your name? ')
-    print('Hello, {0}'.format(name))
+    print_response('Hello, {0}!'.format(name))
+    return name
+
+
+def get_answer(prompt_phrase):
+    """Retrive answer from user.
+
+    Args:
+        prompt_phrase: The phrase shown user as a question.
+
+    Returns:
+        user's answer
+    """
+    return prompt.string(prompt_phrase)
+
+
+def print_response(phrase):
+    """Print phrase to user.
+
+    Args:
+        phrase: The print phrase to user.
+    """
+    print(phrase)
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,14 @@
+# coding=UTF-8
+
+"""Brain Even Game module."""
+
+from brain_games import logics
+
+
+def main():
+    """Point of entry to start program."""
+    logics.brain_even_start()
+
+
+if __name__ == '__main__':
+    main()
